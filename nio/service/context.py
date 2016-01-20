c5359d4ab3b41c172a77fa3b30324b088d719d61
Franky Martin <fmartin@neutral.io>
2016-01-20 09:38:20 -0500

    NIO-770, removing information passed to service that was never used

--- nio/service/context.py
+++ nio/service/context.py
@@ -8,8 +8,7 @@
 class ServiceContext(object):
 
     def __init__(self, properties, blocks=None,
-                 block_router_type=None, router_settings=None,
-                 service_data=None, block_data=None, all_blocks_data=None):
+                 block_router_type=None, router_settings=None):
         """ Initializes information needed for a Service
 
         Arguments:
@@ -26,28 +25,3 @@ class ServiceContext(object):
         self.blocks = blocks if blocks is not None else {}
         self.block_router_type = block_router_type
         self.router_settings = router_settings or {}
-
-        # data core components write to for service
-        self.service_data = service_data or {}
-        # data core components write to for all blocks
-        self.all_blocks_data = all_blocks_data or {}
-        # data core components write to for specific blocks
-        self.block_data = block_data or {}
-
-    def get_service_data(self):
-        """ Allows to retrieve data saved for the service by core components
-        """
-        return self.service_data
-
-    def get_block_data(self, block_type=None):
-        """ Allows to retrieve data saved for the block by core components
-
-        Arguments:
-            block_type (string): if None, all blocks data set by core
-                components is retrieved, otherwise data is retrieved for
-                the specified block_type, where block_type is a string
-                containing the block namespace
-        """
-        if block_type:
-            return self.block_data.get(block_type)
-        return self.all_blocks_data
