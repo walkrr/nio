dd23dcee6354a37d9153eb1fc2b91d534ffd411d
Chris Brackert <cbrackert@gmail.com>
2016-02-24 15:47:01 -0800

    Clean up float property unit tests NIO-814

--- nio/properties/tests/test_float_property.py
+++ nio/properties/tests/test_float_property.py
@@ -1,6 +1,6 @@
 from nio.signal.base import Signal
 from nio.properties import FloatProperty, PropertyHolder
-from nio.util.support.test_case import NIOTestCase
+from nio.util.support.test_case import NIOTestCaseNoModules
 
 
 class ContainerClass(PropertyHolder):
@@ -8,9 +8,10 @@ class ContainerClass(PropertyHolder):
     default = FloatProperty(default=1.23)
 
 
-class TestFloatProperty(NIOTestCase):
+class TestFloatProperty(NIOTestCaseNoModules):
 
     def test_default(self):
+        """Test default behavior of float property."""
         container = ContainerClass()
         self.assertIsNotNone(container.property)
         self.assertEqual(container.default(), 1.23)
@@ -18,6 +19,7 @@ class TestFloatProperty(NIOTestCase):
         #self.assertEqual(container.default.default, 1.23)
 
     def test_expression(self):
+        """Test expressions for file property."""
         container = ContainerClass()
         container.property = "{{ 1 + 2.1 }}"
         self.assertIsNotNone(container.property)
@@ -25,6 +27,7 @@ class TestFloatProperty(NIOTestCase):
         #self.assertEqual(container.default.default, 1.23)
 
     def test_expression_with_signal(self):
+        """Test signal expressions for file property."""
         container = ContainerClass()
         container.property = "{{ $value }}"
         self.assertIsNotNone(container.property)
