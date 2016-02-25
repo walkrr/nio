6e3f740ae7cdb122ddf8e787e1aaccba3a11657b
Chris Brackert <cbrackert@gmail.com>
2016-02-24 16:05:13 -0800

    Clean up object property unit tests NIO-814

--- nio/properties/tests/test_object_property.py
+++ nio/properties/tests/test_object_property.py
@@ -1,7 +1,7 @@
 from nio.signal.base import Signal
 from nio.properties import ObjectProperty, PropertyHolder, \
     StringProperty
-from nio.util.support.test_case import NIOTestCase
+from nio.util.support.test_case import NIOTestCaseNoModules
 
 
 class ContainedClass(PropertyHolder):
@@ -12,14 +12,16 @@ class ContainerClass(PropertyHolder):
     property = ObjectProperty(ContainedClass, default=ContainedClass())
 
 
-class TestObjectProperty(NIOTestCase):
+class TestObjectProperty(NIOTestCaseNoModules):
 
     def test_default(self):
+        """Test default behavior of object property."""
         container = ContainerClass()
         self.assertIsNotNone(container.property)
         self.assertEqual(type(container.property()), ContainedClass)
 
     def test_expression(self):
+        """Test expressions for object property."""
         container = ContainerClass()
         contained = ContainedClass()
         contained.sub_property = "three is {{ 1 + 2 }}"
@@ -28,6 +30,7 @@ class TestObjectProperty(NIOTestCase):
         self.assertEqual(container.property().sub_property(), 'three is 3')
 
     def test_expression_with_signal(self):
+        """Test signal expressions for object property."""
         container = ContainerClass()
         contained = ContainedClass()
         contained.sub_property = "{{ $value }}"
