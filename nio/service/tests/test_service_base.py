09a00702ec1cb9e2b6e981f322c24608129e5619
Franky Martin <fmartin@neutral.io>
2016-03-02 16:20:59 -0500

    NIO-830, improving service base coverage

--- nio/service/tests/test_service_base.py
+++ nio/service/tests/test_service_base.py
@@ -1,11 +1,18 @@
 from unittest.mock import Mock
 
+from nio import Block
 from nio.properties.exceptions import AllowNoneViolation
 from nio.router.base import BlockRouter
 from nio.service.base import Service
 from nio.service.context import ServiceContext
 from nio.signal.base import Signal
 from nio.util.support.test_case import NIOTestCaseNoModules
+
+class Block1(Block):
+    pass
+
+class Block2(Block):
+    pass
 
 
 class TestBaseService(NIOTestCaseNoModules):
@@ -19,6 +26,88 @@ class TestBaseService(NIOTestCaseNoModules):
         ))
         # Make sure the name property got set properly
         self.assertEqual(service.name(), "ServiceName")
+        self.assertIsNotNone(service.get_logger())
+
+    def test_start_stop(self):
+        """Makes sure service and blocks update statuses accordingly """
+        service = Service()
+
+        blocks = [{"type": Block1,
+                   "properties": {'name': 'block1'}},
+                  {"type": Block2,
+                   "properties": {'name': 'block2'}}]
+
+        service.do_configure(ServiceContext(
+            {"name": "ServiceName", "log_level": "WARNING"},
+            blocks=blocks,
+            block_router_type=BlockRouter
+        ))
+        # verify that statuses were updated
+        status = service.full_status()
+        self.assertIn("service", status)
+        self.assertEqual(status["service"], "configured")
+        self.assertIn("block1", status)
+        self.assertEqual(status["block1"], "configured")
+        self.assertIn("block2", status)
+        self.assertEqual(status["block2"], "configured")
+
+        service.do_start()
+
+        # verify that statuses were updated
+        status = service.full_status()
+        self.assertIn("service", status)
+        self.assertEqual(status["service"], "started")
+        self.assertIn("block1", status)
+        self.assertEqual(status["block1"], "started")
+        self.assertIn("block2", status)
+        self.assertEqual(status["block2"], "started")
+
+        self.assertEqual(len(service.blocks), 2)
+
+        service.do_stop()
+
+        # verify that statuses were updated
+        status = service.full_status()
+        self.assertIn("service", status)
+        self.assertEqual(status["service"], "stopped")
+        self.assertIn("block1", status)
+        self.assertEqual(status["block1"], "stopped")
+        self.assertIn("block2", status)
+        self.assertEqual(status["block2"], "stopped")
+
+    def test_commands(self):
+        """ Asserts commands functionality """
+        service = Service()
+
+        service.do_configure(ServiceContext(
+            {"name": "ServiceName", "log_level": "WARNING"},
+            block_router_type=BlockRouter
+        ))
+
+        service.do_start()
+
+        # check get_description and assert that info matches expectations
+        description = service.get_description()
+        self.assertIn("properties", description)
+        self.assertIn("name", description["properties"])
+        self.assertIn("auto_start", description["properties"])
+
+        self.assertIn("commands", description)
+        self.assertIn("start", description["commands"])
+        self.assertIn("stop", description["commands"])
+        self.assertIn("status", description["commands"])
+        self.assertIn("heartbeat", description["commands"])
+        self.assertIn("runproperties", description["commands"])
+
+        # verify heartbeat command
+        self.assertEqual(service.heartbeat().name, "started")
+
+        # verify runproperties command
+        run_properties = service.runproperties()
+        self.assertIn("name", run_properties)
+        self.assertEqual(run_properties["name"], "ServiceName")
+
+        service.do_stop()
 
     def test_config_with_no_name(self):
         """Make sure a service cononfig has required 'name' property."""
