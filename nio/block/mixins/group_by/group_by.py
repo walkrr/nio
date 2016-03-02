14affa3477094ba7a12cbc6732d9576a078f6062
mattdodge <matt@n.io>
2016-03-01 17:19:14 -0800

    NIO-827 Add helper method to group by mixin

--- nio/block/mixins/group_by/group_by.py
+++ nio/block/mixins/group_by/group_by.py
@@ -10,14 +10,17 @@ class GroupBy(object):
 
     Use NIO's native expression syntax to configure a value by which to
     group signals. In your block's 'process_signals' method, pass the
-    list of signals and a target method to GroupBy.for_each_group. The
+    list of signals and a target method to self.for_each_group. The
     signals will be grouped automatically and the target method called
     on each group in turn.
 
-    Note that the particular groups are recorded in GroupBy._groups. If
-    you'd like to map a function over all the groups outside of
-    process_signals, just call GroupBy.for_each_group without a second
-    argument.
+    If you'd like to map a function over all the groups outside of
+    process_signals, just call self.for_each_group without passing any
+    signals.
+
+    You can also make use of the default process_signals implmentation of
+    grouping all incoming signals and processing them independently by not
+    overriding process_signals but instead overriding process_group_signals.
 
     Properties:
         group_by: The expression by which signals will be grouped.
@@ -28,42 +31,15 @@ class GroupBy(object):
 
     def __init__(self):
         super().__init__()
-        self._dirty = False
-        self._groups = []
+        self._groups = set()
 
-    def group(self, signals):
-        """ Groups the provided objects by self.group_by.
+    def for_each_group(self, target, signals=None, *args, **kwargs):
+        """ Execute a function once for every group
 
-        Return:
-            out (dict): A dictionary where keys are group names/keys and values
-            are lists of signals
-        """
-        signal_groups = defaultdict(list)
-        for s in signals:
-            try:
-                key = self.group_by(s)
-            except:
-                self._logger.exception(
-                    "GroupBy expression failed, ignoring signal")
-                continue
-
-            # Need to make sure that the key is a hashable object
-            if not isinstance(key, Hashable):
-                self._logger.warning(
-                    "{} is not hashable cannot be grouped by. Using str "
-                    "representation instead".format(key))
-                key = str(key)
-
-            # Add the key to our list of groups, if it's not already there
-            if key not in self._groups:
-                self._groups.append(key)
-
-            signal_groups[key].append(s)
-
-        return signal_groups
-
-    def for_each_group(self, target, signals=None, kwargs=None):
-        """ Maps 'target' over the calculated signal groups.
+        If signals are provided, the target function will only be called
+        for groups that appear in the provided signals. If signals are omitted,
+        the function will be called for every group that has ever been
+        processed in this block.
 
         Note that the required template for 'target' depends on
         whether or not signals are provided for grouping.
@@ -73,17 +49,15 @@ class GroupBy(object):
             signals (list): The list of signals to be grouped. If
                 this is left off, the list of recorded groups is
                 used.
-            kwargs : kwargs to be passed to target
+            args, kwargs : Additional arguments to be passed to target
 
         Returns:
-            output (list): A list of the return values of each call to the
-            target function. If the target function returns a list, the output
-            list will be a concatenation of results.
-            So [1,2,3,4,5] not [[1,2],[3],[4,5]]
+            list: A list of the return values of each call to the
+                target function. If the target function returns a list, the
+                output list will be a concatenation of results.
+                So [1,2,3,4,5] not [[1,2],[3],[4,5]]
         """
-        kwargs = kwargs if kwargs else {}
         output = []
-
         # if there are no signals, assume that the target function has
         # only one parameter, the group key
         if signals is None:
@@ -91,27 +65,75 @@ class GroupBy(object):
             # and produce the output along the way.
             # We need to send a copy of self._groups so that in case the target
             # function alters the groups list it doesn't affect iteration
-            [self._output_append(output, target(k, **kwargs))
-             for k in copy(self._groups)]
+            for group in copy(self._groups):
+                result = target(group, *args, **kwargs)
+                if result and isinstance(result, list):
+                    output.extend(result)
 
         # otherwise, assume that the target function has two arguments,
         # the signal and its group key
         else:
-            [self._output_append(output, target(val, key, **kwargs))
-             for key, val in self.group(signals).items()]
+            for group, group_sigs in self._group_signals(signals).items():
+                result = target(group_sigs, group, *args, **kwargs)
+                if result and isinstance(result, list):
+                    output.extend(result)
 
         return output
 
-    def _output_append(self, target_list, value):
-        """ Append a value to a target list.
+    def process_signals(self, signals, input_id=None):
+        """A convenient implementation of process signals for grouping blocks.
 
-        If value is a list, don't append the list, but rather extend the list
-        with the list in value.
+        This implementation of process_signals is provided only as a
+        convenience to block developers. It prevents having to write the same
+        code every time for process signals when in reality you only care about
+        the signals after they are grouped.
+
+        To use this, don't implement process_signals, but rather implement
+        process_group_signals according to the signature provided in this file.
+        Any signals you return from that method will be notified out of the
+        block on the default output.
+
+        If you wish to not use this implementation, just implement your own
+        version of process_signals.
+        """
+        output_sigs = self.for_each_group(
+            self.process_group_signals, signals, input_id)
+        self.notify_signals(output_sigs)
+
+    def process_group_signals(self, signals, group, input_id=None):
+        """Implement this method to make use of the default behavior.
+
+        If you wish to make use of the default implementation of
+        process_signals then override this method. If you wish, you may return
+        a list of signals out of this method to have them notified out of
+        the block.
+        """
+        pass  # pragma: no cover
+
+    def _group_signals(self, signals):
+        """ Groups the provided signals according to the configuration
+
+        Args:
+            signals (list): A list of signals to group
 
         Returns:
-            None - list append/extend happens in place
+            dict: A dictionary where keys are group names and values are lists
+                of the signals in that group
         """
-        if isinstance(value, list):
-            target_list.extend(value)
-        elif value:
-            target_list.append(value)
+        signal_groups = defaultdict(list)
+        for s in signals:
+            key = self.group_by(s)
+
+            # Need to make sure that the key is a hashable object
+            if not isinstance(key, Hashable):
+                self._logger.warning(
+                    "{} is not hashable cannot be grouped by. Using str "
+                    "representation instead".format(key))
+                key = str(key)
+
+            # Add the key to our set of groups and the signal to the list
+            # corresponding to the group
+            self._groups.add(key)
+            signal_groups[key].append(s)
+
+        return signal_groups
