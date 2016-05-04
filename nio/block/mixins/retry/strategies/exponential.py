74b897cb621b599bfbba439302310c85faec46a6
mattdodge <matt@n.io>
2016-05-04 16:32:26 -0700

    NIO-884 Add exponential retry strategy

--- nio/block/mixins/retry/strategies/exponential.py
+++ nio/block/mixins/retry/strategies/exponential.py
@@ -4,35 +4,15 @@ from nio.block.mixins.retry.strategy import BackoffStrategy
 
 class ExponentialBackoff(BackoffStrategy):
 
-    def __init__(self, max_retries=-1, multiplier=1):
-        """ Create an instance of a linear backoff strategy
+    def wait_for_retry(self):
+        """Sleep a certain number of seconds before we do the retry.
 
-        Args:
-            max_retries (int): The maximum number of retries to make before
-                giving up on retrying.
-            multiplier (float): The multiplier to apply when sleeping. By
-                default, this strategy will sleep a number of seconds equal to
-                the current retry number. This argument allows you to alter
-                that. For example, by default the 5th retry will sleep for 5
-                seconds. Passing a multiplier of 1.5 will cause the 5th retry
-                to sleep for 7.5 seconds instead.
+        We will wait the multiplier times 2 raised to one less than the
+        current retry number.
+        So the first retry will be mult*1, then mult*2, then mult*4, etc
+
+        The exception is when our current retry is greater than our max
+        number of retries, then we want to use the max number of retries.
+        This can happen if the strategy is configured to run indefinitely.
         """
-        super().__init__()
-        self._max_retries = max_retries
-        self._multiplier = multiplier
-
-    def next_retry(self):
-        """Sleep a certain number of seconds unless we are out of retries."""
-        # Check if we have a maximum number of retries and we've passed it
-        self.logger.info("Executing retry number {}, max retries is {}".format(
-            self.retry_num, self._max_retries))
-        if self._max_retries >= 0 and self.retry_num > self._max_retries:
-            self.logger.warning(
-                "Retry number {} exceeds the  max retries of {}".format(
-                    self.retry_num, self._max_retries))
-            return False
-
-        # We will wait the current retry number times the multiplier,
-        # so first retry will be 1*mult, second will be 2*mult, etc
-        sleep(self.retry_num * self._multiplier)
-        return True
+        sleep(self.multiplier * 2**(min(self.retry_num, self.max_retry)-1))
---
+++ nio/block/mixins/retry/strategies/tests/test_exponential.py
@@ -0,0 +1,109 @@
+from unittest.mock import patch
+from nio import Block
+from nio.block.mixins.retry.retry import Retry
+from nio.testing.block_test_case import NIOBlockTestCase
+
+
+class ExponentialBackoffBlock(Retry, Block):
+    """ An example of a block that uses a Exponential Backoff strategy """
+    pass
+
+
+class TestExponentialBackoff(NIOBlockTestCase):
+
+    def assert_next_retry_sleeps_for(self, block, num_seconds):
+        """Make sure that given a failure the block will sleep for some time"""
+        sleep_path = 'nio.block.mixins.retry.strategies.exponential.sleep'
+        with patch(sleep_path) as sleep:
+            block._backoff_strategy.request_failed(Exception())
+            if block._backoff_strategy.should_retry():
+                block._backoff_strategy.wait_for_retry()
+            sleep.assert_called_once_with(num_seconds)
+
+    def test_default(self):
+        """Test the default behavior of the exponential backoff"""
+        block = ExponentialBackoffBlock()
+        self.configure_block(block, {
+            "retry_options": {
+                "strategy": "exponential"
+            }
+        })
+        # First failure should sleep for 1 second
+        self.assert_next_retry_sleeps_for(block, 1)
+        # Second failure should sleep for 2 seconds
+        self.assert_next_retry_sleeps_for(block, 2)
+        # Third failure should sleep for 3 seconds
+        self.assert_next_retry_sleeps_for(block, 4)
+        # Success should reset, so next failure will be 1 second
+        block._backoff_strategy.request_succeeded()
+        self.assert_next_retry_sleeps_for(block, 1)
+
+    def test_multiplier(self):
+        """Test that we can multiply the number of seconds to sleep"""
+        block = ExponentialBackoffBlock()
+        self.configure_block(block, {
+            "retry_options": {
+                "strategy": "exponential",
+                "multiplier": 3.14
+            }
+        })
+        # Execute 3 retries and make sure we multiply each time
+        self.assert_next_retry_sleeps_for(block, 3.14)
+        self.assert_next_retry_sleeps_for(block, 6.28)
+        self.assert_next_retry_sleeps_for(block, 12.56)
+        self.assert_next_retry_sleeps_for(block, 25.12)
+
+    def test_indefinite(self):
+        """Test that retries can happen indefinitely """
+        block = ExponentialBackoffBlock()
+        self.configure_block(block, {
+            "retry_options": {
+                "strategy": "exponential",
+                "max_retry": 4,
+                "indefinite": True
+            }
+        })
+        # Should count to three retries then keep trying, every 2**3 seconds
+        self.assert_next_retry_sleeps_for(block, 1)
+        self.assert_next_retry_sleeps_for(block, 2)
+        self.assert_next_retry_sleeps_for(block, 4)
+        self.assert_next_retry_sleeps_for(block, 8)
+        self.assert_next_retry_sleeps_for(block, 8)
+        self.assert_next_retry_sleeps_for(block, 8)
+
+    def test_multiplied_indefinite(self):
+        """Test that retries can happen indefinitely """
+        block = ExponentialBackoffBlock()
+        self.configure_block(block, {
+            "retry_options": {
+                "strategy": "exponential",
+                "max_retry": 3,
+                "multiplier": 3,
+                "indefinite": True
+            }
+        })
+        # Should count to three retries then keep trying, every 3*2**2 seconds
+        self.assert_next_retry_sleeps_for(block, 3)
+        self.assert_next_retry_sleeps_for(block, 6)
+        self.assert_next_retry_sleeps_for(block, 12)
+        self.assert_next_retry_sleeps_for(block, 12)
+        self.assert_next_retry_sleeps_for(block, 12)
+
+    def test_max_retries(self):
+        """Test that we can cap the number of retries"""
+        block = ExponentialBackoffBlock()
+        self.configure_block(block, {
+            "retry_options": {
+                "strategy": "exponential",
+                "max_retry": 2
+            }
+        })
+        # Execute 3 retries, but make sure only the first two actually sleep
+        self.assert_next_retry_sleeps_for(block, 1)
+        self.assert_next_retry_sleeps_for(block, 2)
+        # Last retry should return false and not sleep
+        sleep_path = 'nio.block.mixins.retry.strategies.exponential.sleep'
+        with patch(sleep_path) as sleep:
+            block._backoff_strategy.request_failed(Exception())
+            self.assertFalse(block._backoff_strategy.should_retry())
+            sleep.assert_not_called()
