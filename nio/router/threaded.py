4de78ce24d74a73e9ddc4a749db7fce55706ed81
Franky Martin <fmartin@neutral.io>
2016-05-06 17:25:59 -0400

    NIO-888, removing redundant code

--- nio/router/threaded.py
+++ nio/router/threaded.py
@@ -9,12 +9,4 @@ class ThreadedBlockRouter(BlockRouter):
 
     def deliver_signals(self, block_receiver, signals):
         """ Delivers signals to given block in a new thread """
-        spawn(self._safe_process_signals, block_receiver, signals)
-
-    def _safe_process_signals(self, block_receiver, signals):
-        try:
-            self.notify_signals_to_block(block_receiver, signals)
-        except:
-            self.status.add(RunnerStatus.error)
-            self.logger.exception(
-                "{0}.process_signals failed".format(block_receiver.block.name))
+        spawn(self.notify_signals_to_block, block_receiver, signals)
