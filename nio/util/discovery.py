d28b513513a8d5e4ecf612cca4801469874d9fa9
Franky Martin <fmartin@neutral.io>
2016-07-22 10:38:31 -0400

    NIO-918, unifying 'discoverability' class attribute as one, adding default discoverability to be used when class not marked with a discoverability-like decorator

--- nio/util/discovery.py
+++ nio/util/discovery.py
@@ -3,9 +3,6 @@
 Classes marked as `discoverable` allow the system to identify them and register
 them, while marking a class as  `not_discoverable` produces the opposite effect
 
-n.io will look for services and components marked as `discoverable` and for
-blocks marked as `not_discoverable`
-
 Note: A class being marked `discoverable` will not mark its subclasses as
 `discoverable`. Each subclass must be explicitly marked with the `discoverable`
 decorator, the same applies for `not_discoverable`
@@ -25,7 +22,7 @@ To mark a class as not_discoverable, use the parameter-less decorator
 def discoverable(_class):
     """The decorator method to be called on the class object.
 
-    This method will set the proper discoverable type to the class. It
+    This method will set the proper `discoverable` type to the class. It
     should return the class passed in, according to the decorator spec.
     """
 
@@ -35,7 +32,20 @@ def discoverable(_class):
     return _class
 
 
-def is_class_discoverable(_class):
+def not_discoverable(_class):
+    """The decorator method to be called on the class object.
+
+    This method will set the proper `not discoverable` type to the class. It
+    should return the class passed in, according to the decorator spec.
+    """
+
+    # Set the attribute to the class name, to prevent subclasses from also
+    # being not discoverable.
+    setattr(_class, _get_discoverable_attribute(_class), False)
+    return _class
+
+
+def is_class_discoverable(_class, default_discoverability=False):
     """ Returns true if the class is marked discoverable
 
     Note: discoverability does not extend to subclasses. Each subclass must
@@ -43,46 +53,16 @@ def is_class_discoverable(_class):
 
     Args:
         _class (class): The class in question, should be a class object
+        default_discoverability: when attribute is not specified, this value
+            will be provided.
 
     Returns:
         bool: True if the class is discoverable
     """
-    return bool(getattr(_class, _get_discoverable_attribute(_class), False))
+    return bool(getattr(_class, _get_discoverable_attribute(_class),
+                        default_discoverability))
 
 
 def _get_discoverable_attribute(_class):
     """ Get an attribute to set on a class to consider it discoverable """
     return "__{}_is_discoverable".format(_class.__name__)
-
-
-def not_discoverable(_class):
-    """The decorator method to be called on the class object.
-
-    This method will set the proper not discoverable type to the class. It
-    should return the class passed in, according to the decorator spec.
-    """
-
-    # Set the attribute to the class name, to prevent subclasses from also
-    # being not discoverable.
-    setattr(_class, _get_not_discoverable_attribute(_class), True)
-    return _class
-
-
-def is_class_not_discoverable(_class):
-    """ Returns true if the class is marked not discoverable
-
-    Note: discoverability does not extend to subclasses. Each subclass must
-    explicitly be marked as not discoverable
-
-    Args:
-        _class (class): The class in question, should be a class object
-
-    Returns:
-        bool: True if the class is not discoverable
-    """
-    return bool(getattr(_class, _get_not_discoverable_attribute(_class), False))
-
-
-def _get_not_discoverable_attribute(_class):
-    """ Get an attribute to set on a class to consider it not discoverable """
-    return "__{}_not_discoverable".format(_class.__name__)
--- nio/util/tests/test_discoverable.py
+++ nio/util/tests/test_discoverable.py
@@ -1,5 +1,6 @@
 from nio.testing.test_case import NIOTestCaseNoModules
-from nio.util.discovery import discoverable, is_class_discoverable
+from nio.util.discovery import discoverable, not_discoverable, \
+    is_class_discoverable
 from nio.block.base import Block
 
 
@@ -8,19 +9,62 @@ class DiscoverableBlock(Block):
     pass
 
 
-class NonDiscoverableSubBlock(DiscoverableBlock):
+class NotSpecifiedSubBlock(DiscoverableBlock):
     pass
 
 
 @discoverable
-class DiscoverableSubBlock(NonDiscoverableSubBlock):
+class DiscoverableSubBlock(NotSpecifiedSubBlock):
+    pass
+
+
+@not_discoverable
+class NotDiscoverableBlock(Block):
+    pass
+
+
+@not_discoverable
+class NotDiscoverableSubBlock(Block):
+    pass
+
+
+@not_discoverable
+class NotDiscoverableInheritingBlock(DiscoverableBlock):
+    pass
+
+
+@discoverable
+class DiscoverableInheritingBlock(NotDiscoverableBlock):
     pass
 
 
 class TestDiscoverable(NIOTestCaseNoModules):
 
-    def test_subclass(self):
-        """Tests that subclasses are not discoverable"""
+    def test_is_class_discoverable(self):
+
+        # asserting simple discoverable and not_discoverable decorators
         self.assertTrue(is_class_discoverable(DiscoverableBlock))
-        self.assertFalse(is_class_discoverable(NonDiscoverableSubBlock))
+        self.assertFalse(is_class_discoverable(NotDiscoverableBlock))
+        # assert discoverability does not come from default discoverability
+        self.assertTrue(is_class_discoverable(DiscoverableBlock, False))
+        self.assertFalse(is_class_discoverable(NotDiscoverableBlock, True))
+
+        # assert default discoverability when not provided
+        self.assertFalse(is_class_discoverable(NotSpecifiedSubBlock))
+
+        # show that a non-specified block would go either way depending on
+        # the default discoverability setting
+        self.assertTrue(is_class_discoverable(NotSpecifiedSubBlock, True))
+        self.assertFalse(is_class_discoverable(NotSpecifiedSubBlock, False))
+
+        # assert straightforward inheriting blocks specifying own decorators
         self.assertTrue(is_class_discoverable(DiscoverableSubBlock))
+        self.assertFalse(is_class_discoverable(NotDiscoverableSubBlock))
+        # assert discoverability does not come from default discoverability
+        self.assertTrue(is_class_discoverable(DiscoverableSubBlock, False))
+        self.assertFalse(is_class_discoverable(NotDiscoverableSubBlock, True))
+
+        # as expected a block specifying opposite-to-parent discoverability
+        # overrides
+        self.assertTrue(is_class_discoverable(DiscoverableInheritingBlock))
+        self.assertFalse(is_class_discoverable(NotDiscoverableInheritingBlock))
