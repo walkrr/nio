2354555f71b259bdf581acb7c9f7d642d170ac12
Franky Martin <fmartin@neutral.io>
2018-06-07 16:26:08 -0400

    NIO-1105, updating testing module with new interface

--- nio/testing/modules/communication/publisher.py
+++ nio/testing/modules/communication/publisher.py
@@ -8,14 +8,28 @@ class Publisher(object):
     def __init__(self, topic=None, **kwargs):
         self.topic = topic
 
-    def open(self):
+    def open(self, on_connected=None, on_disconnected=None):
         """ Opens publishing channel
+
+        Args:
+            on_connected (callable): function receiving notification when
+                connection is established
+            on_disconnected (callable): function receiving notification when
+                a disconnection occurs
 
         This method delegates functionality to notify others the
         publisher creation and its definitions via a management signal.
 
         """
         PubSubManager.add_publisher(self)
+        # on_connected, on_disconnected is not applicable,
+        # testing communication module is always connected
+
+    def is_connected(self):
+        """ Determine if this publisher is connected and ready
+
+        """
+        return True
 
     def send(self, signals):
         """ Sends signals
