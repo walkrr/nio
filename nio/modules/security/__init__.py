6561f1d0e3cca0911b9516f96ca07958371f7f6b
mattdodge <matt@n.io>
2018-10-01 14:57:43 -0700

    Create CoreServiceAccount user

--- nio/modules/security/__init__.py
+++ nio/modules/security/__init__.py
@@ -1,4 +1,4 @@
 from nio.modules.security.authorizer import Authorizer, Unauthorized
 from nio.modules.security.authenticator import Authenticator
-from nio.modules.security.user import User
+from nio.modules.security.user import User, CoreServiceAccount
 from nio.modules.security.task import SecureTask
